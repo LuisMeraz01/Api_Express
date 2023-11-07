d1f9fabaad48ba205e41a4984d722196907762ac
LuisMeraz01227 <luis_mbs05@hotmail.com>
2023-11-07 10:01:43 -0700

    Modificacion con ejemplos de rutas de consumo

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('Hola mundo!');
+    res.send('mi primera API');
+});
+
+app.get('/saludo', function(req, res){
+    res.send('Hola mundo');
+});
+
+app.get('/despedida', function(req, res){
+    res.send('Adios mundo cruel');
+});
+
+app.get('/despedida/familia', function(req, res){
+    res.send('Los quiero mucho a todos');
+});
+
+app.get('/despedida/empleados', function(req, res){
+    res.send('Ojala se rompan una pierna');
 });
 
 app.listen(3000, function(){
